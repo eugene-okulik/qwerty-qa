318166b8b287471135e873111ee73917fd521e21
Peter qwerty-qa <myemail@mail.com>
2025-08-16 20:52:23 +0300

    Codestyle changed according to Flake8 rules. Whitespace around arithmetic operator added.

--- homework/peter_qwertyqa/homework3/task3.py
+++ homework/peter_qwertyqa/homework3/task3.py
@@ -1,4 +1,4 @@
 a, b = map(float, input().split())
 
-print("Arithmetic average: " + str((a+b)/2))
-print("Geometric average: " + str((a*b)**0.5))
+print("Arithmetic average: " + str((a + b) / 2))
+print("Geometric average: " + str((a * b) ** 0.5))
