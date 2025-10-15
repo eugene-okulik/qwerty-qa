12ac482d8ca5efbfc22dfb5a4169ce8231f6a810
Peter qwerty-qa <myemail@mail.com>
2025-10-16 00:00:46 +0300

    Homework 10 solutions added

---
+++ homework/peter_qwertyqa/homework10/task4.py
@@ -0,0 +1,12 @@
+PRICE_LIST = '''тетрадь 50р
+книга 200р
+ручка 100р
+карандаш 70р
+альбом 120р
+пенал 300р
+рюкзак 500р'''
+
+items = PRICE_LIST.split()
+price_dict = {items[i]: int(items[i+1][:-1]) for i in range(0, len(items), 2)}
+
+print(price_dict)
